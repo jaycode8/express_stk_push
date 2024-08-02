14280145cf7a2ef1348ac4e612c85be37860a83b
jaycode8 <jamesmumo443@gmail.com>
2024-08-02 17:13:40 +0300

    updates

--- controlers/mpesa.controller.js
+++ controlers/mpesa.controller.js
@@ -12,19 +12,12 @@ const password = new Buffer.from(shortcode + passkey + timestamp).toString("base
 const home = (req, res) => {
     const now = new Date();
     const readableTimestamp = now.toISOString();
-    // console.log(readableTimestamp);
-    generateToken();
     res.json({ msg: "hello there" });
 };
 
 const stk = async (req, res) => {
     const phone = req.body.phone.substring(1);
     const amount = req.body.amount;
-    
-    // const shortcode = process.env.MPESA_PAYBILL;
-    // const passkey = process.env.MPESA_PASSKEY;
-
-    // const password = new Buffer.from(shortcode + passkey + timestamp).toString("base64");
     
     await axios
         .post(
@@ -38,7 +31,7 @@ const stk = async (req, res) => {
                 PartyA: `254${phone}`,
                 PartyB: shortcode,
                 PhoneNumber: `254${phone}`,
-                CallBackURL: `https://f85d-2c0f-fe38-220b-f25-5266-41b3-ea61-d31a.ngrok-free.app/callback/`,
+                CallBackURL: `https://11d2-154-79-248-82.ngrok-free.app/callback/`,
                 AccountReference: `254${phone}`,
                 TransactionDesc: "Test for app jay",
             },
@@ -86,20 +79,22 @@ const transactions = async (req, res) => {
 };
 
 const b2c = async (req, res) => {
+    const phone = req.body.phone.substring(1);
+    const amount = req.body.amount;
     await axios
         .post(
             "https://sandbox.safaricom.co.ke/mpesa/b2c/v3/paymentrequest",
             {
-                OriginatorConversationID: "feb5e3f2-fbbc-4745-844c-ee37b546f627",
+                OriginatorConversationID: "a592ce17-642e-417e-b6b4-38da16b059fb",
                 InitiatorName: "testapi",
                 SecurityCredential: "32SzVdmCvjpmQfw3X2RK8UAv7xuhh304dXxFC5+3lslkk2TDJY/Lh6ESVwtqMxJzF7qA==",
                 CommandID: "BusinessPayment",
-                Amount: "10",
-                PartyA: "600996",
-                PartyB: "254728762287",
+                Amount: amount,
+                PartyA: "600426",
+                PartyB: `254${phone}`,
                 Remarks: "here are my remarks",
-                QueueTimeOutURL: "https://a593-154-79-248-82.ngrok-free.app/b2ctimeout",
-                ResultURL: "https://a593-154-79-248-82.ngrok-free.app/b2cresults",
+                QueueTimeOutURL: "https://11d2-154-79-248-82.ngrok-free.app/b2ctimeout",
+                ResultURL: "https://11d2-154-79-248-82.ngrok-free.app/b2cresults",
                 Occassion: "Christmas",
             },
             {
@@ -113,14 +108,13 @@ const b2c = async (req, res) => {
             res.status(200).json(data.data);
         })
         .catch((err) => {
-            console.log("-------------------------------error-----------------------------------");
             console.log(err);
             res.status(400).json(err.message);
         });
 }
 
 const b2cresults = async (req, res) => {
-    console.log(req.body.Result.ResultParameters);
+    console.log(req.body.Result.ReferenceData);
 }
 
 const b2ctimeOut = async (req, res) => {
@@ -133,9 +127,9 @@ const balanceEnquiry = async (req, res) => {
             "https://sandbox.safaricom.co.ke/mpesa/accountbalance/v1/query",
             {
                 Initiator: "testapi",
-                SecurityCredential: password,
-                "Command ID": "AccountBalance",
-                PartyA: "600979",
+                SecurityCredential: "SAFVNChNHfVtXEZMBuVo+a1Hwr+DtrUVN3zVg==",
+                CommandID: "AccountBalance",
+                PartyA: "600992",
                 IdentifierType: "4",
                 Remarks: "ok",
                 QueueTimeOutURL: "https://a593-154-79-248-82.ngrok-free.app/b2ctimeout",
